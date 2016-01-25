650afabac3779c2047fc2307f871d0d89f254165
Ádám Gólya <adam.stork@gmail.com>
2016-01-25 16:24:10 +0100

    feat(server.js): add koa-router to server
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var koa = require('koa');
 var serve = require('koa-static');
+var router = require('koa-router')();
 var path = require('path');
 var app = koa();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.use(serve(path.join(__dirname, '../www/public/')));
 
