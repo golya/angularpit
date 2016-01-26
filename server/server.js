0b122c70b51fa049d75fc43dc6e092fda60f5e71
Ádám Gólya <adam.stork@gmail.com>
2016-01-26 16:44:12 +0100

    feat(server.js): export the whole app for the tests
    The webserver will be started if it has no parent module, this check is
    necessary at the tests where we will include the app and it will be
    started by the tests.
    
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ var koa = require('koa');
 var serve = require('koa-static');
 var router = require('koa-router')();
 var path = require('path');
-var app = koa();
+var app = module.exports = koa();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.use(serve(path.join(__dirname, '../www/public/')));
 
-app.listen(3000);
+if (!module.parent) {
+    app.listen(3000);
+}
