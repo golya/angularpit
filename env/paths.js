296717f700a66bea443574ba3652600fbc45d13a
Ádám Gólya <adam.stork@gmail.com>
2016-01-30 19:38:50 +0100

    feat(paths.js): initial add of paths
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

---
+++ env/paths.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports.server_path = './server/';
+module.exports.databases_path = server_path + 'databases/';
+module.exports.www_path = 'www/';
+module.exports.public_path = www_path + 'public/';
